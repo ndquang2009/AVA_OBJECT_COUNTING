3345a46203508cc55b2c8f23aed51f6c7bb7fc3c
quangnguyen <ndquang2009@gmail.com>
2023-09-09 14:05:34 +0800

    Added live counting function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from datetime import datetime
-
+import json
+# ------------------- Login function --------------------------------------
 def dologin(base_url, username, password):
     url = str(base_url) + "/api/v1/dologin"
     account = {
@@ -11,14 +12,14 @@ def dologin(base_url, username, password):
     if response.status_code == 200:
         print("OK!")
         return response.cookies
-    else
+    else:
         print("Login failed", response.status_code)
-def convert_iso_time():
-    current_datetime = datetime.utcnow()
-    iso8601_time = current_datetime.isoformat()
-    iso8601_time = iso8601_time.split(".")[0] + "Z"
-    print(iso8601_time)
+# ------------------- Get all counting areas information -----------------
 
+# --------------------Print a beautify json string ----------------------
+def json_print(json_string):
+    pretty_json = json.dumps(json_string, indent=4)
+    print(pretty_json)
 def get_countingAreas(base_url, cookies):
     url = str(base_url) + "/api/v1/countingAreas"
     print(url)
@@ -29,7 +30,7 @@ def get_countingAreas(base_url, cookies):
     else:
         print("HTTP error code:", response.status_code)
         return response.status_code
-
+# ------------------ Reformat all counting areas information -------------------
 def get_id_name_type_countingAreas(countingAreas):
     countingAreas_list = []
     countingAreas_profile = {
@@ -39,24 +40,25 @@ def get_id_name_type_countingAreas(countingAreas):
     }
     #print(countingAreas)
     length = len(countingAreas)
-    print(length)
+    # print(length)
     for i in range(0,length):
         countingAreas_profile = {
             "countingAreas_id": countingAreas[i]["id"],
             "countingAreas_name": countingAreas[i]["name"],
             "countingAreas_type": countingAreas[i]["type"]
         }
-        #print(countingAreas_profile)
         countingAreas_list.append(countingAreas_profile)
     return countingAreas_list
 
-
-def get_live_counting(base_url, countingArea_id,cookies):
+# ------------------ Perform live counting on a specify area -------------------------
+def get_live_counting(base_url, countingArea_id, cookies):
     url = str(base_url) + "/api/v1/countingAreas/{}/counts".format(countingArea_id)
-    current_iso_time = convert_iso_time()
+    current_datetime = datetime.utcnow()
+    current_iso8601_time = current_datetime.isoformat()
+    current_iso8601_time = current_iso8601_time.split(".")[0] + "Z"
     params = {
         "end": "",
-        "start": current_iso_time,
+        "start": current_iso8601_time,
         "step": "",
         "time_location": "Asia/Ho_Chi_Minh"
     }
@@ -67,4 +69,36 @@ def get_live_counting(base_url, countingArea_id,cookies):
         return temp
     else:
         print("HTTP code error:", response.status_code)
+
+# -------------------- Main program -------------------------------------------------------
+# -------------------- Declare deployment and account information -------------------------
+base_url = "https://msi-apac.au1.aware.avasecurity.com"
+username = "quang.nguyen@avigilon.com"
+password = "Av1gilon@pac"
+
+# Login to get the authentication cookies
+auth_cookies = dologin(base_url,username,password)
+
+# Get all counting areas information
+counting_areas_list = get_countingAreas(base_url, auth_cookies)
+
+# Issue new data records with area name - id - type
+counting_areas_id = get_id_name_type_countingAreas(counting_areas_list)
+
+#Print the new list
+json_print(counting_areas_id)
+
+# Get area id from user input
+user_input = input("Please copy the area ID and paste here:")
+
+# Perform live counting
+tmp = get_live_counting(base_url, str(user_input), auth_cookies)
+print("Current person:", tmp["totals"][0]["countPerson"])
+
+
+
+
+
+
+
 
